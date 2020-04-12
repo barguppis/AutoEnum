844842f4bcdf46c9ef8904d0b9bcb4bfad4aaf69
Aidan Rivera <57341367+barguppis@users.noreply.github.com>
2020-04-12 03:38:14 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Updating AutoEnum..."
-cd ..
-rm -r AutoEnum
+mkdir ../AutoEnumBackups
+mv Scans ../AutoEnumBackups
+mv Responder ../AutoEnumBackups
+rm -r ../AutoEnum && cd ..
 git clone https://github.com/barguppis/AutoEnum.git
+cd AutoEnum && ls
 echo "Done, be sure to refresh the directory in your terminal to see new files"
 sleep 3
